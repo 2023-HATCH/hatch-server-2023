d6fd1ecc111a254acd4e61a615cafeb69de5fa46
Soobin-329 <soobin400@gmail.com>
2023-03-25 14:44:30 +0900

    Add: Initial Commit (HH-35)

---
+++ src/main/java/hatch/hatchserver2023/HatchServer2023Application.java
@@ -0,0 +1,13 @@
+package hatch.hatchserver2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatchServer2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatchServer2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/hatch/hatchserver2023/HatchServer2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package hatch.hatchserver2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatchServer2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
