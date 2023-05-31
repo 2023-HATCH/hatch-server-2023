9dfd70e6c2ea3bea85e3f2823e5ebd6cf282a5df
jeeheaG <jeehea99@naver.com>
2023-05-31 21:07:32 +0900

    Fix : Pushed in a wrong directory (HH-29)

--- src/main/java/hatch/hatchserver2023/domain/HealthCheckController.java
+++ src/main/java/hatch/hatchserver2023/domain/HealthCheckController.java
@@ -1,5 +1,13 @@
 package hatch.hatchserver2023.domain;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 @RestController
-public class TestController {
+public class HealthCheckController {
+    @GetMapping
+    public String  healthCheck() {
+        return "Health check ok";
+    }
 }
