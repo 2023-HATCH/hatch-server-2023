ba1782c8de0cf8500b54c3c1c88793c87d1c5d5e
jeeheaG <jeehea99@naver.com>
2023-06-12 14:57:12 +0900

    Update : Change response format of healthCheck api to json

--- src/main/java/hatch/hatchserver2023/domain/HealthCheckController.java
+++ src/main/java/hatch/hatchserver2023/domain/HealthCheckController.java
@@ -1,5 +1,8 @@
 package hatch.hatchserver2023.domain;
 
+import hatch.hatchserver2023.global.common.response.CommonResponse;
+import hatch.hatchserver2023.global.common.response.code.CommonCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
     @GetMapping
-    public String  healthCheck() {
-        return "Pocket Pose API Server : Health check ok. Server is running";
+    public ResponseEntity<CommonResponse> healthCheck() {
+        return ResponseEntity.ok()
+                .body(CommonResponse.toResponse(CommonCode.OK, "Pocket Pose API Server : Health check ok. Server is running"));
     }
 }
