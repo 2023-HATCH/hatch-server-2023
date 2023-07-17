8769f78a75102294b56028bedf55611359a154bb
Soobin-329 <soobin400@gmail.com>
2023-07-17 17:12:39 +0900

    Add: Validation on CommentRequestDto (HH-213)

--- src/main/java/hatch/hatchserver2023/domain/video/api/CommentController.java
+++ src/main/java/hatch/hatchserver2023/domain/video/api/CommentController.java
@@ -14,6 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -42,7 +43,7 @@ public class CommentController {
     @PostMapping("/{videoId}")
     public ResponseEntity<?> registerComment(@AuthenticationPrincipal User user,
                                              @PathVariable UUID videoId,
-                                             @RequestBody CommentRequestDto request){
+                                             @RequestBody @Valid CommentRequestDto request){
 
         Comment comment = commentService.createComment(request.getContent(), videoId, user);
 
--- src/main/java/hatch/hatchserver2023/domain/video/dto/CommentRequestDto.java
+++ src/main/java/hatch/hatchserver2023/domain/video/dto/CommentRequestDto.java
@@ -2,9 +2,12 @@ package hatch.hatchserver2023.domain.video.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class CommentRequestDto {
 
+    @NotBlank
     private String content;
 
     public CommentRequestDto(String content){
