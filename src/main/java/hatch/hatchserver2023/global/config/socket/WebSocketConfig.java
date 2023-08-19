1adf554fce419f86ec814ba0cc3f61da046da306
jeeheaG <jeehea99@naver.com>
2023-08-19 20:25:41 +0900

    Fix : Fix test failed. Use @SentToUser instead of SimpMessingTemplate

--- src/main/java/hatch/hatchserver2023/global/config/socket/WebSocketConfig.java
+++ src/main/java/hatch/hatchserver2023/global/config/socket/WebSocketConfig.java
@@ -3,6 +3,7 @@ package hatch.hatchserver2023.global.config.socket;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
