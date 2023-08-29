1a345a91a787ecd350cc25fada3aa67869c09b9f
jeeheaG <jeehea99@naver.com>
2023-08-29 16:37:41 +0900

    FIX : Change to get key file by absolute path

--- src/main/java/hatch/hatchserver2023/global/config/firebase/FcmInitializer.java
+++ src/main/java/hatch/hatchserver2023/global/config/firebase/FcmInitializer.java
@@ -12,9 +12,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 @Slf4j
 @Component
@@ -33,8 +31,17 @@ public class FcmInitializer {
         // 리눅스에서 에러남
 //        InputStream inputStream = new ByteArrayInputStream(keyJson.getBytes()); // String 으로 받은 환경변수값 json 을 inputstream 으로 변형
 
-        ClassPathResource resource = new ClassPathResource(filePath);
-        InputStream inputStream = resource.getInputStream();
+        // resoutces 파일 바로 접근할 수 있는 방법임 -> 리눅스에서 불가
+//        ClassPathResource resource = new ClassPathResource(filePath);
+//        InputStream inputStream = resource.getInputStream();
+
+        // 절대경로로 파일 읽어오기
+        String basePath = new File("").getAbsolutePath();
+        log.info("basePath : {}", basePath);
+        String fileAbsolutePath = basePath + filePath;
+        log.info("fileAbsolutePath : {}", fileAbsolutePath);
+
+        InputStream inputStream = new FileInputStream(new File(fileAbsolutePath));
 
         GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
 
